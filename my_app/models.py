279d5282d399048ae07741170c5f6257cf668eb7
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-10-07 20:23:58 +0300

    add Customer model

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=256)
+    surname = models.CharField(max_length=256)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"Customer {self.name} {self.surname}"
+
+    class Meta:
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+        ordering = ["name"]
+
