ebe8aa197cb31f4bf750f1089dcdeb52f7493f6f
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-10-18 21:47:12 +0300

    add funk 'home page'

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from my_app.models import Customer, Room, Order
 # Create your views here.
+
+def home_page(request):
+    context = {
+        "render_string": "This is home page. Hello!!"
+    }
+
+    return render(
+        request=request, template_name="booking/home-page.html", context=context
+    )
+
