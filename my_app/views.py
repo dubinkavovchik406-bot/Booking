54115e023e285fc8f6cfb91cbefa6684d4244de3
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-10-19 23:01:54 +0300

    add new funk order_form

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
+
 from my_app.models import Customer, Room, Order
-# Create your views here.
 
 def home_page(request):
     context = {
@@ -30,3 +31,37 @@ def room_detail(request, r_id):
     return render(
         request=request, template_name="booking/room-detail.html", context=context
     )
+
+def order_form(request, r_id):
+    if request.method == "POST":
+        customer_id = request.POST.get("customer-id")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            customer = get_object_or_404(Customer, id=customer_id)
+            room = get_object_or_404(Room, id=r_id)
+        except ValueError:
+            return HttpResponse(
+                "Wrong value for customer id or room number!",
+                status=400
+            )
+        except Http404:
+            return HttpResponse(
+                "Customer or room not found",
+                status=404
+            )
+
+        order = Order.objects.create(
+            customer=customer,
+            room=room,
+            start_time=start_time,
+            end_time=end_time
+        )
+        return redirect("order-form-details", pk=order.id)
+    else:
+        context = {
+            'room_id': r_id
+        }
+        return render(request=request, template_name="booking/order-form.html", context=context)
+
