5dc5b2e3148d8ea3db9c08ee40f3331849fd46a8
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-11-01 17:34:22 +0200

    some changes

--- my_app/models.py
+++ my_app/models.py
@@ -1,18 +1,6 @@
 from django.db import models
-
+from booking_system import settings
 # Create your models here.
-class Customer(models.Model):
-    name = models.CharField(max_length=256)
-    surname = models.CharField(max_length=256)
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return f"Customer {self.name} {self.surname}"
-
-    class Meta:
-        verbose_name = "Customer"
-        verbose_name_plural = "Customers"
-        ordering = ["name"]
 
 class Room(models.Model):
     number = models.IntegerField()
@@ -31,14 +19,14 @@ class Room(models.Model):
         ordering = ["number"]
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders")
     room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="orders")
     start_time = models.DateField()
     end_time = models.DateField()
     creations_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.customer.name} {self.customer.surname} in Room #{self.room.number}"
+        return f"{self.user.username} in Room #{self.room.number}"
 
     class Meta:
         verbose_name = "Order"
